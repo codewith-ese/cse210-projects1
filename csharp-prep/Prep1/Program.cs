f032e4beff21642ed23a53c63620fa1c86409b98
codewith-ese <eeseinone@gmail.com>
2024-06-12 19:08:18 +0100

    Prep 1 Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // This will ask the user for first and last name. 
+        Console.Write("what is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name ? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
+        Console.WriteLine();
+        
     }
 }
+
+
+
