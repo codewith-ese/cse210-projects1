31cc03aff081150d81e6dcc8989349c42ff9069e
codewith-ese <eeseinone@gmail.com>
2024-06-13 00:28:44 +0100

    Prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,53 +5,51 @@ class Program
     static void Main(string[] args)
     {
         // This program will ask users for numbers 
-        Console.Write(" What  is your grade percentage?");
+        Console.Write(" What  is your grade percentage? ");
         string valueFromUser = Console.ReadLine();
 
         int userGrade = int.Parse(valueFromUser);
 
+        string letterGrade = "";
+
         if (userGrade >=90)
         {
-            Console.WriteLine();
-            Console.WriteLine("Wow!,  Execcellent! ");
-            Console.WriteLine("Congratulations you have got an 'A' ");
-            Console.WriteLine($" your total score is {userGrade}%");
-            Console.WriteLine();
+            letterGrade ="A";
         }
         else if (userGrade >= 80)
         {
-            Console.WriteLine();
-            Console.WriteLine("Congratulations you have got a 'B'  ");
-            Console.WriteLine($" your total score is {userGrade}%");
-            Console.WriteLine();
+            letterGrade = "B";
         }
         else if (userGrade >= 70)
         {
-            Console.WriteLine();
-            Console.WriteLine(" Congratulations you have got a 'C' ");
-            Console.WriteLine($" your total score is {userGrade}%");
-            Console.WriteLine();
+            letterGrade = "C";
+
         }
 
         else if (userGrade >= 60)
         {
-            Console.WriteLine();
-            Console.WriteLine(" Congratulations you have got a 'D' ");
-            Console.WriteLine($" your total score is {userGrade}%");
-            Console.WriteLine();
+            letterGrade = "D";
         }
-        else if (userGrade < 60)
-        {
-            Console.WriteLine();
-            Console.WriteLine(" sory you have got an F which means you failed the course");
-            Console.WriteLine($" your total score is {userGrade}% ");
-            Console.WriteLine();
-        }
+
         else
         {
-            Console.WriteLine();
-            Console.WriteLine(" Invalid Entry ");
-            Console.WriteLine();
+           letterGrade = "F";
+        }
+
+        Console.WriteLine("===============================");
+        Console.WriteLine($" Your grade is {userGrade}% ");
+
+        if (userGrade >=70)
+        {
+            Console.WriteLine(" You Passed! ");
+            Console.WriteLine("============================");
+
+        }
+
+        else
+        {
+            Console.WriteLine(" Better luck next time ");
+            Console.WriteLine("===============================");
         }
 
 
