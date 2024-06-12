d6d34f3ac72cf3281fea61f27ca24ec57331e7e2
codewith-ese <eeseinone@gmail.com>
2024-06-12 21:26:40 +0100

    Prep 2 first step completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // This program will ask users for numbers 
+        Console.Write(" What  is your grade percentage?");
+        string valueFromUser = Console.ReadLine();
+
+        int userGrade = int.Parse(valueFromUser);
+
+        if (userGrade >=90)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Wow!,  Execcellent! ");
+            Console.WriteLine("Congratulations you have got an 'A' ");
+            Console.WriteLine($" your total score is {userGrade}%");
+            Console.WriteLine();
+        }
+        else if (userGrade >= 80)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Congratulations you have got a 'B'  ");
+            Console.WriteLine($" your total score is {userGrade}%");
+            Console.WriteLine();
+        }
+        else if (userGrade >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Congratulations you have got a 'C' ");
+            Console.WriteLine($" your total score is {userGrade}%");
+            Console.WriteLine();
+        }
+
+        else if (userGrade >= 60)
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Congratulations you have got a 'D' ");
+            Console.WriteLine($" your total score is {userGrade}%");
+            Console.WriteLine();
+        }
+        else if (userGrade < 60)
+        {
+            Console.WriteLine();
+            Console.WriteLine(" sory you have got an F which means you failed the course");
+            Console.WriteLine($" your total score is {userGrade}% ");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Invalid Entry ");
+            Console.WriteLine();
+        }
+
+
     }
 }
