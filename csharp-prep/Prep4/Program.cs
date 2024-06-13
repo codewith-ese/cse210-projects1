93dd424781bb74d24f986cce57e9918c4606b0f8
codewith-ese <eeseinone@gmail.com>
2024-06-13 15:40:20 +0100

    prep 4 first commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,56 @@
 using System;
-
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    { 
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+
+        int enterNumber = -1;
+
+        while  (enterNumber != 0)
+        {
+
+            Console.Write(" Enter number: ");
+            enterNumber = int.Parse(Console.ReadLine());
+
+            numbers.Add(enterNumber);
+
+          //  foreach (int number in numbers)
+           // {
+           //     Console.WriteLine(number);
+          //  }
+
+            if (enterNumber == 0)
+            {   
+                    // Calculate the sum total in the list 
+                int sum = numbers.Sum();
+                Console.WriteLine("==================");
+                Console.WriteLine($" The sum is: {sum} ");
+                Console.WriteLine("===================");
+
+                    // Calculate the average 
+                double average = numbers.Average();
+                Console.WriteLine("==================");
+                Console.WriteLine($" The Average is: {average} ");
+                Console.WriteLine("===================");
+        
+
+                    // Calculating the gratest value
+                numbers.Sort();
+                int greatestValue = numbers[numbers.Count-1];
+                Console.WriteLine("==================");
+                Console.WriteLine($" The largest number is: {greatestValue} ");
+                Console.WriteLine("===================");
+                Console.WriteLine();
+
+
+            }
+
+        }
+
     }
 }
