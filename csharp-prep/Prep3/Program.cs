53ff0d53d654ca973d2e3c0e19d5107b9a653213
codewith-ese <eeseinone@gmail.com>
2024-06-13 10:47:43 +0100

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       // This code will generate random numbers 
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1, 40);
+
+       //int magicNumber = 12;
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+
+            //  Ask for user guess number
+            Console.Write(" What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine(" Lower ");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine(" Higher ");
+            }
+
+            else
+            {
+                Console.WriteLine(" You guess it! ");
+            }
+
+        }
     }
 }
