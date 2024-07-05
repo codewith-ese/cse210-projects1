3993366723c3c2094158553d94c1ab92463fe70d
codewith-ese <eeseinone@gmail.com>
2024-07-05 13:28:04 +0100

    edited

--- prepare/Learning04/WritenAssignment.cs
+++ prepare/Learning04/WritenAssignment.cs
@@ -2,7 +2,7 @@ public class WritingAssignment : Assignment
 {
     private string _title;
 
-    // Notice the syntax here that the WritingAssignment constructor has 3 parameters and then
+    // This WritingAssignment constructor has 3 parameters and then
     // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
     public WritingAssignment(string studentName, string topic, string title)
         : base(studentName, topic)
