d959a906296669d45bf035553997b04d8e012c5c
codewith-ese <eeseinone@gmail.com>
2024-07-22 12:24:55 +0100

    Foundation 2 product ordering system completed

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -35,7 +35,7 @@ public class Order
         string packingLabel = "";
         foreach (Product product in _products)
         {
-            packingLabel += $"\n Name: {product.SetName()}, Product ID: {product.ProductId()} ";
+            packingLabel += $"\n Product-Name: {product.SetName()}, Product ID: {product.ProductId()} ";
         }
 
         return packingLabel;
@@ -56,96 +56,3 @@ public class Order
         _customer = customer;
     }
 }
-
-
-
-
-// using System.Numerics;
-
-// public class Order
-// {
-//     private List<Product> _product;
-//     private Customer _customer;
-//      private double  _price;
-//      private int _quantity;
-
-//     public Order()
-//     {
-//         _product = new List<Product>();
-//     }
-//     public double CalculateTotal()
-//     {
-//         double totalSum = 0;
-//         foreach (Product product in _product)
-//         {
-//             totalSum += product.Total();
-
-//             //var sum = totalSum * product.Total();
-//         }
-        
-//         if (_customer.Usa())
-//         {
-//             totalSum += 5; // Shipping cost for customers in the USA
-//         }
-//         else
-//         {
-//             totalSum += 35; // Shipping cost for customers outside the USA
-//         }
-//         return totalSum;
-//     }
-
-//     public double CalculateNumbeOfOrder()
-//     {
-//         double total = 0;
-//         foreach (Product product in _product)
-//         {
-//             total += product.Total();
-
-//             //var sum = total * product.Total();
-//         }
-        
-//         if (_customer.Usa())
-//         {
-//             total += 5; // Shipping cost for customers in the USA
-//         }
-//         else
-//         {
-//             total += 35; // Shipping cost for customers outside the USA
-//         }
-//         return total;
-//     }
-
-//     public string PackingLabel()
-//     {
-//         string packingLabel = "";
-//         foreach (Product product in _product)
-//         {
-//             packingLabel += $"Name: {product.Name()}, Product ID: {product.ProductId}\n";
-//         }
-//         return packingLabel;
-//     }
-    
-//     public string ShippingLabel()
-//     {
-//         return $" {_customer} {_product}  ";
-//     }
-
-
-//     public void addProduct(Product product)
-//     {
-//         List<Product> products = new List<Product>();
-
-//         foreach (Product product1 in products)
-//         {
-//             product1.DisplayProductInfo();
-//         }
-
-//     }
-
-//     public void addCustomer(Customer customer)
-//     {
-//         _customer = customer;
-
-    
-//     }
-// }
